6bf5620a54932844aac22657292ce9305374035b
Paride Legovini <pl@ninthfloor.org>
2019-05-04 16:06:59 +0200

    Default to 80 columns output if call to stty fails
    ubuntu-bug-triage outputs a table whose width is based on the width of
    the terminal the tool is called from. Fallback to a reasonable default
    when ubuntu-bug-triage is not called from a terminal (i.e. the call to
    stty(1) fails).

--- ubuntu_bug_triage/view.py
+++ ubuntu_bug_triage/view.py
@@ -87,7 +87,12 @@ class TerminalView(BaseView):
         """Initialize terminal view."""
         super().__init__()
 
-        _, self.term_columns = os.popen('stty size', 'r').read().split()
+        try:
+            _, self.term_columns = os.popen('stty size 2>/dev/null', 'r').read().split()
+        except:
+            # Fallback to a reasonable default if `stty size` fails, e.g. when
+            # the script not called from a terminal.
+            self.term_columns = 80
 
         # The table has a set number characters that always exist:
         #     4 for '|' boarders
