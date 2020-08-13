2e8c31d5314f4e28660c030602aeff637d4bd070
Joshua Powers <josh.powers@canonical.com>
2020-08-13 15:51:27 -0700

    launchpadlib: pull in newer version
    During first login the following trace showed up, which was fixed in
    LP: #1861873
    
        AttributeError: module 'cgi' has no attribute 'parse_qs'
    
    Fixes #14

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(README_FILE, "r") as readme:
 
 setup(
     name="ubuntu-bug-triage",
-    version="20.1",
+    version="20.2",
     description="Recently updated Launchpad bugs for triage",
     long_description=README_TEXT,
     long_description_content_type="text/markdown",
