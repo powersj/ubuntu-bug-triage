521df571e8b7542f082221a7f90b8e227fe39112
Joshua Powers <powersj@fastmail.com>
2021-03-29 18:36:20 -0700

    release 21.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(README_FILE, "r") as readme:
 
 setup(
     name="ubuntu-bug-triage",
-    version="20.2",
+    version="21.1",
     description="Recently updated Launchpad bugs for triage",
     long_description=README_TEXT,
     long_description_content_type="text/markdown",
